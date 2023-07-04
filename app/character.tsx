bea69437833f3fc20a8c1e64a194c4b2028f22e8
Scott Dodge <scott@scotato.com>
2023-07-03 22:37:57 -0700

    add colors, backgrounds, hats, chaos background mode, intensity mode, on chain text

--- app/character.tsx
+++ app/character.tsx
@@ -1,41 +1,89 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
+
+const ROWS = 11;
+const COLS = 23;
+const FONTSIZE = 8;
+const FONTWEIGHT = 900;
+const CELLWIDTH = (FONTSIZE * 3) / 4;
+const CELLHEIGHT = (FONTSIZE * 3) / 2;
+const CANVASPADDINGX = 8;
+const CANVASPADDINGY = 8;
+const CANVASWIDTH = COLS * CELLWIDTH + CANVASPADDINGX * 2;
+const CANVASHEIGHT = ROWS * CELLHEIGHT + CANVASPADDINGY * 2;
+
+const COLORBLACK = "#000000";
+const COLORWHITE = "#FFFFFF";
+const COLORGRAY = "#A9BFD7";
+const COLORGREEN = "#00BA73";
+const COLORLIGHTBLUE = "#00C9FF";
+const COLORBLUE = "#0080FF";
+const COLORPURPLE = "#AD43ED";
+const COLORPINK = "#FE63FF";
+const COLORORANGE = "#FF8633";
+const COLORRED = "#FE0000";
+
+const characterColors = [
+  COLORGRAY,
+  COLORGREEN,
+  COLORLIGHTBLUE,
+  COLORBLUE,
+  COLORPURPLE,
+  COLORPINK,
+  COLORORANGE,
+  COLORRED,
+];
 
 const heads = [
   ["|", "|"],
   ["[", "]"],
   ["(", ")"],
+  ["{", "}"],
 ];
 
 const eyes = [
-  ["*", "*"],
-  ["$", "$"],
-  ["z", "z"],
-  [".", "."],
-  ["o", "o"],
-  ["o", "O"],
-  ["O", "o"],
-  ["O", "O"],
-  ["0", "0"],
-  ["#", "#"],
-  ["+", "+"],
-  ["'", "'"],
-  ["”", "”"],
-  ["^", "^"],
-  ["=", "="],
-  ["-", "-"],
-  ["T", "T"],
-  ["x", "x"],
-  ["X", "X"],
+  "*",
+  "$",
+  "z",
+  ".",
+  "o",
+  "O",
+  "0",
+  "#",
+  "+",
+  "'",
+  '"',
+  "^",
+  "=",
+  "-",
+  "T",
+  "x",
+  "X",
 ];
 
 const mouths = ["-"];
+
+const hats = [
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "~",
+  "+",
+  "-",
+  "'",
+  ".",
+  "=",
+];
 
 const arms = [
   ["/", "\\"],
   ["<", ">"],
   ["~", "~"],
-  ["{", "}"],
   ["J", "L"],
   ["2", "7"],
 ];
@@ -43,6 +91,7 @@ const arms = [
 const bodies = [
   ["[", "]"],
   ["(", ")"],
+  ["{", "}"],
 ];
 
 const chests = [" ", ":", "*", "=", "."];
@@ -56,16 +105,32 @@ const legs = [
 
 const feet = [["_", "_"]];
 
-export default function Character() {
+const backgrounds = ["-", "=", "+", "*", "#", "/", "\\", "|", "·"];
+
+const firstText = ["O", "N"];
+const lastText = ["C", "H", "A", "I", "N"];
+
+interface CharacterProps {
+  characterColor?: string;
+  patternColor?: string;
+  backgroundColor?: string;
+}
+
+export default function Character({
+  patternColor = "rgba(0, 0, 0, 0.05)",
+  backgroundColor = "white",
+}: CharacterProps) {
   const [didMount, setDidMount] = useState(false);
-  const [headLeft, headRight] = randomFromArray(heads);
-  const [eyeLeft, eyeRight] = randomFromArray(eyes);
-  const mouth = randomFromArray(mouths);
-  const [armLeft, armRight] = randomFromArray(arms);
-  const [bodyLeft, bodyRight] = randomFromArray(bodies);
-  const chest = randomFromArray(chests);
-  const [legLeft, legRight] = randomFromArray(legs);
-  const [footLeft, footRight] = randomFromArray(feet);
+  const character = randomCharacter();
+  const color = randomFromArray(characterColors);
+  const background = randomFromArray(backgrounds);
+  const chaosBackgroundMode = Math.random() > 0.75;
+  const intensityBackgroundMode = Math.random() > 0.75;
+  const intensityBackground = Math.random();
+
+  // center the 2d character matrix on the 2d canvas matrix
+  const x = Math.floor((COLS - character[0].length) / 2);
+  const y = Math.floor((ROWS - character.length) / 2);
 
   useEffect(() => {
     setDidMount(true);
@@ -74,29 +139,85 @@ export default function Character() {
   if (!didMount) return null;
 
   return (
-    <svg viewBox="0 0 64 64">
-      <rect x="0" y="0" width="64" height="64" fill="white" />
-      <text x="24" y="28px" fontSize={4} fontWeight={900}>
-        {headLeft}
-        {eyeLeft}
-        {mouth}
-        {eyeRight}
-        {headRight}
-      </text>
-      <text x="24" y="34px" fontSize={4} fontWeight={900}>
-        {armLeft}
-        {bodyLeft}
-        {chest}
-        {bodyRight}
-        {armRight}
-      </text>
-      <text x="24" y="40px" fontSize={4} fontWeight={900}>
-        {footLeft}
-        {legLeft} {legRight}
-        {footRight}
-      </text>
+    <svg viewBox={`0 0 ${CANVASWIDTH} ${CANVASHEIGHT}`}>
+      <rect
+        x="0"
+        y="0"
+        width={CANVASWIDTH}
+        height={CANVASHEIGHT}
+        fill={backgroundColor}
+      />
+      {Array.from({ length: ROWS }, (_, row) => (
+        <Fragment key={row}>
+          {Array.from({ length: COLS }, (_, col) => {
+            // if its intensityBackgroundMode, we roll a dice and see if its less than the intensityBackground
+            const isCharacterPart =
+              row >= y &&
+              row < y + character.length &&
+              col >= x &&
+              col < x + character[0].length;
+            const characterPart = isCharacterPart
+              ? character[row - y][col - x]
+              : "";
+            let symbol =
+              characterPart ||
+              (intensityBackgroundMode
+                ? Math.random() < intensityBackground
+                  ? chaosBackgroundMode
+                    ? randomFromArray(backgrounds)
+                    : background
+                  : ""
+                : chaosBackgroundMode
+                ? randomFromArray(backgrounds)
+                : background);
+
+            // logic to set the symbol to the characters in firstText if its the first row and lastText if its the last row
+            if (row === 0) {
+              symbol = firstText[col] || symbol;
+            } else if (row === ROWS - 1 && col > col - lastText.length) {
+              symbol = lastText[lastText.length - COLS + col] || symbol;
+            }
+
+            return (
+              <text
+                x={col * CELLWIDTH + CELLWIDTH / 2 + CANVASPADDINGX}
+                y={row * CELLHEIGHT + CELLHEIGHT / 2 + CANVASPADDINGY}
+                fontSize={FONTSIZE}
+                fontWeight={FONTWEIGHT}
+                fill={characterPart ? color : patternColor}
+                textAnchor="middle"
+                dominantBaseline="middle"
+                key={`${row}-${col}`}
+              >
+                {symbol}
+              </text>
+            );
+          })}
+        </Fragment>
+      ))}
     </svg>
   );
+}
+
+function randomCharacter() {
+  const hasHat = Math.random() > 0.75;
+  const hasChest = Math.random() > 0.5;
+  const [headLeft, headRight] = randomFromArray(heads);
+  const eye = randomFromArray(eyes);
+  const mouth = randomFromArray(mouths);
+  const hat = hasHat ? randomFromArray(hats) : "";
+  const [armLeft, armRight] = randomFromArray(arms);
+  const [bodyLeft, bodyRight] = randomFromArray(bodies);
+  const chest = hasChest ? randomFromArray(chests) : "";
+  const [legLeft, legRight] = randomFromArray(legs);
+  const [footLeft, footRight] = randomFromArray(feet);
+
+  return [
+    ["", "", hat, "", ""],
+    [headLeft, eye, mouth, eye, headRight],
+    [armLeft, bodyLeft, chest, bodyRight, armRight],
+    [footLeft, legLeft, "", legRight, footRight],
+  ];
 }
 
 function randomFromArray<T>(array: T[]): T {
